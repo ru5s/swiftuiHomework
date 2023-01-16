2c26bb7342dec43cf44739ceea3bcfb89d2ca259
Ruslan <hellperock@gmail.com>
2023-01-16 22:36:28 +0500

    Initial Commit

---
+++ homework_sc_module_43/homework_sc_module_43App.swift
@@ -0,0 +1,17 @@
+//
+//  homework_sc_module_43App.swift
+//  homework_sc_module_43
+//
+//  Created by Ruslan Ismailov on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct homework_sc_module_43App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
